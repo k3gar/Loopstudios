a4322f7aee835c4d4ab3e74c4f783fa072668834
Kevin Garcia <kevinede40@gmail.com>
2021-09-16 21:46:22 -0600

    Start working

---
+++ loopstudios/src/components/Prueba.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Prueba = () => {
+    return (
+        <div>
+            <h1>PRUEBAAAAA</h1>
+        </div>
+    )
+}
+
+export default Prueba
--- loopstudios/src/components/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function myHeader() {
-    return(
-        <h1>Prueba</h1>
-    )
-}
-
-export default myHeader
